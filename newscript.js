4d22597526cf01a5b7596503d8b27a8c76204561
Dan D <degeare28@gmail.com>
2019-11-28 08:08:35 -0700

    basic logic for quiz

---
+++ newscript.js
@@ -0,0 +1,150 @@
+//Variables = qNumber(null), timer(num), score(num), initials(text)
+let timer = 90;
+let score = 0;
+let initials = "";
+let qNumber;
+
+//DOM Objects = START BUTTON, ANSWER BUTTONS, QUESTION CONTAINER, QUESTION ELEMENT
+const startButton = document.getElementById("startButton");
+const qContainer = document.getElementById("questionsContainer");
+const qElement = document.getElementById("question");
+const answerButtons = document.getElementById("answers");
+const countdown = document.getElementById("timerArea");
+
+startButton.addEventListener("click", startGame);
+
+//function to start the game
+//called when start button is clicked, should run the function to display questions and the function to start the timer
+
+function startGame() {
+  startButton.classList.add("hide");
+  qNumber = 0;
+  qContainer.classList.remove("hide");
+  startClock();
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+  showQuestion(questions[qNumber]);
+}
+
+//function to display the questions
+//should load one object from the questions array into the proper html elements, then run the function to collect answers
+function showQuestion(question) {
+  qElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtons.appendChild(button);
+  });
+}
+
+//function to start the timer
+//should run a countdown that is displayed in the HTML, when time is up, should run the game over function
+function startClock() {
+  countdown.innerHTML = "Time Remaining: " + timer;
+  if (timer <= 0) {
+    gameOver();
+  } else {
+    timer -= 1;
+    runningTimer = setTimeout(startClock, 1000);
+  }
+}
+
+//function to collect answers
+//should listen for what answer the user clicks on, compare it to the correct answer, and decrease the timer if wrong. should then run the next question function
+//unless the current question is the last, then it should run the game over function
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  if (!selectedButton.dataset.correct) {
+    score = score - 10;
+  }
+  if (qNumber == questions.length - 1) {
+    gameOver();
+  } else {
+    clearQuestion();
+    qNumber++;
+    showQuestion(questions[qNumber]);
+    console.log(score);
+  }
+}
+
+//function to clear the current question
+//should empty the HTML elements that are occupied with the currently displayed question
+function clearQuestion() {
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+//function for game over
+//should grab the current time remaining and set it as the score, hide the questions area, display the score to the user, and give them the chance to try again or submit
+//their high scores via a text box for intials and the high scores function
+function gameOver() {
+  clearInterval(runningTimer);
+  countdown.innerHTML = "Finished";
+  clearQuestion();
+  showResults();
+  startButton.innerText = "Restart";
+  startButton.classList.remove("hide");
+  timer = 90;
+  score = 0;
+}
+
+function showResults() {
+  qElement.innerHTML = "Your score is: " + (timer + score);
+}
+
+//function to submit high scores
+//should grab the users score and initials and add it to the high scores object, ranked numerically, and run the function to display the high scores
+
+//HIGH SCORES PAGE
+//BUTTONS = CLEAR SCORES(FUNCTION) && GO BACK(LINK)
+
+//function to display high scores
+//should populate the HTML with a ranked display of the high scores and and provide the option to clear the scores via a function
+
+//function to clear high scores
+//should fire on click, and erase the values of the high scores object
+
+/////
+//Questions go Here
+/////
+const questions = [
+  {
+    question: "What is 2 + 2?",
+    answers: [
+      { text: "4", correct: true },
+      { text: "22", correct: false }
+    ]
+  },
+  {
+    question: "Who is the best YouTuber?",
+    answers: [
+      { text: "Web Dev Simplified", correct: true },
+      { text: "Traversy Media", correct: true },
+      { text: "Dev Ed", correct: true },
+      { text: "Fun Fun Function", correct: true }
+    ]
+  },
+  {
+    question: "Is web development fun?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false }
+    ]
+  },
+  {
+    question: "What is 4 * 2?",
+    answers: [
+      { text: "6", correct: false },
+      { text: "8", correct: true }
+    ]
+  }
+];
