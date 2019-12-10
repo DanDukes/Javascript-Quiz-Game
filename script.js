a93b5d6cfa23330ece49a1db12130cec178e44b3
Dan D <degeare28@gmail.com>
2019-12-10 07:41:36 -0700

    Updated Questions to live versions

--- script.js
+++ script.js
@@ -161,35 +161,83 @@ function clearScores() {
 /////
 const questions = [
   {
-    question: "What is 2 + 2?",
+    question: "Inside which HTML element do we put the JavaScript?",
     answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false }
+      { text: "javascript", correct: false },
+      { text: "script", correct: true },
+      { text: "js", correct: false}
+      { text: "jQuery", correct: false}
     ]
   },
   {
-    question: "Who is the best YouTuber?",
+    question: "Where is the correct place to insert JavaScript?",
     answers: [
-      { text: "Web Dev Simplified", correct: true },
-      { text: "Traversy Media", correct: true },
-      { text: "Dev Ed", correct: true },
-      { text: "Fun Fun Function", correct: true }
+      { text: "The Head Section", correct: false },
+      { text: "The Body Section", correct: false },
+      { text: "In an External File", correct: false },
+      { text: "All of the Above", correct: true }
     ]
   },
   {
-    question: "Is web development fun?",
+    question: "The external JavaScript file must contain the script tag.",
     answers: [
-      { text: "Kinda", correct: false },
-      { text: "YES!!!", correct: true },
-      { text: "Um no", correct: false },
-      { text: "IDK", correct: false }
+      { text: "True", correct: false },
+      { text: "False", correct: true }
+      
     ]
   },
   {
-    question: "What is 4 * 2?",
+    question: 'How do you write "Hello World" in an alert box?',
     answers: [
-      { text: "6", correct: false },
-      { text: "8", correct: true }
+      { text: 'msg("Hello World");', correct: false },
+      { text: 'prompt("Hello World");', correct: false },
+      { text: 'alertBox("Hello World");', correct:false },
+      { text: 'alert("Hello World");', correct: true}
     ]
-  }
+  },
+  {
+    question: 'How do you create a function in JavaScript?',
+    answers: [
+      { text: 'function myFunction()', correct: true },
+      { text: 'function = myFunction()', correct: false },
+      { text: "make.function.myFunction()", correct:false },
+      { text: 'function:myFunction()', correct: false}
+    ]
+  },
+  {
+    question: 'How do you call a function named "myFunction"?',
+    answers: [
+      { text: "call myFunction()", correct: false },
+      { text: "read myFunction()", correct: false },
+      { text: "myFunction()", correct:true },
+      { text: "run.myFunction()", correct: false}
+    ]
+  },
+  {
+    question: "How do you write an IF statement in JavaScript?",
+    answers: [
+      { text: "if (i === 5)", correct: true },
+      { text: "if i = 5 then", correct: false },
+      { text: "if i === 5 then", correct:false },
+      { text: "if (i = 5)", correct: false}
+    ]
+  },
+  {
+    question: "!= means what in javascript?",
+    answers: [
+      { text: 'Or', correct: false },
+      { text: 'And', correct: false },
+      { text: "Plus and Equal To", correct:false },
+      { text: 'Not Equal To', correct: true}
+    ]
+  },
+  {
+    question: 'What Characters Contains an Array?',
+    answers: [
+      { text: '< >', correct: false },
+      { text: '{ }', correct: false },
+      { text: "[ ]", correct:true },
+      { text: '# #', correct: false}
+    ]
+  },
 ];
